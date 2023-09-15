bf601f041775c04ff0f60cb4ecd6bf2b19d7cd6d
Bella Trinanda Sanni <bellatrinanda08@gmail.com>
2023-09-15 21:57:44 +0700

    update

--- js/login_register.js
+++ js/login_register.js
@@ -1,21 +1,24 @@
+// Mengambil elemen tombol sign-in dan sign-up
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+// Mengambil elemen container
 const container = document.querySelector(".container");
+// Mengambil elemen tombol sign-in dan sign-up yang kedua
 const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
-
+// Menambahkan event listener untuk tombol sign-up
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
 });
-
+// Menambahkan event listener untuk tombol sign-in
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
 });
-
+// Menambahkan event listener untuk tombol sign-up yang kedua
 sign_up_btn2.addEventListener("click", () => {
     container.classList.add("sign-up-mode2");
 });
-
+// Menambahkan event listener untuk tombol sign-in yang kedua
 sign_in_btn2.addEventListener("click", () => {
     container.classList.remove("sign-up-mode2");
 });
