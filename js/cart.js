df1b9f118753c1071b6c9df3ec5a83ba4df00169
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-10-08 13:20:48 +0700

    update penyesuaian ui

--- js/cart.js
+++ js/cart.js
@@ -9,30 +9,6 @@ cartButton.addEventListener('click', () => {
     }
 });
 
-const product = [
-    {
-        id: 0,
-        image: 'assets/Cheeseburger.jpg',
-        title: 'Cheese Burger',
-        detail: 'A mouthwatering beef patty grilled to perfection, adorned with melty cheese, served on a soft, toasted bun with fresh lettuce, tomato, and our special sauce.',
-        price: 65,
-    },
-    {
-        id: 1,
-        image: 'assets/BurgerBacon.jpg',
-        title: 'Burger bacon',
-        detail: 'A juicy, flame-grilled patty topped with crispy bacon strips, lettuce, tomato, and our signature sauce, all served on a warm, toasted bun.',
-        price: 75,
-    },
-    {
-        id: 2,
-        image: 'assets/BurgerAyam.jpg',
-        title: 'Chicken Burger',
-        detail: 'Enjoy our Chicken Burger, featuring a tender and perfectly seasoned chicken patty, topped with fresh lettuce, ripe tomatoes.',
-        price: 55,
-    },
-];
-
 // Tambahan JavaScript untuk tombol close
 const closeButton = document.getElementById("closeButton");
 
@@ -40,9 +16,9 @@ closeButton.addEventListener('click', () => {
     cartContainer.classList.add('hidden');
 });
 
-const categories = [...new Set(product.map((item)=>
-    {return item}))]
-    let i=0;
+// const categories = [...new Set(product.map((item)=>
+//     {return item}))]
+//     let i=0;
 
 // document.getElementsByClassName('card-container')[0].innerHTML = categories.map((item)=>
 // {
@@ -62,7 +38,7 @@ const categories = [...new Set(product.map((item)=>
 //     )
 // }).join('')
 
-var cart=[];
+// var cart=[];
 
 function addtocart(a){
     cart.push({...categories[a]});
@@ -75,34 +51,34 @@ function delElement(a){
 }
 
 //Menampilkan cart sesuai dengan  pesanan dari user
-function displaycart() {
-    let j = 0, total = 0;
-    document.getElementById("cart-count").innerHTML = cart.length;
-    if (cart.length == 0) {
-        document.getElementById('cartItem').innerHTML = "The cart is empty";
-        document.getElementById("total").innerHTML = "Rp " +0+ ".000";
-        document.getElementById("cart-count").innerHTML = "";
-    } else {
-        document.getElementById('cartItem').innerHTML = cart.map((items) => 
-        {
-            var {image, title, price} = items;
-            total=total+price;
-            document.getElementById("total").innerHTML = "Rp " + total + ".000";
-            return (
-                `<div class='cart-item'>
-                    <div class='row-img'>
-                        <img class='rowimg' src=${image}>
-                    </div>
-                    <p style='font-size: 15px;'>${title}</p>
-                    <h2 style='font-size: 18px;'>Rp ${price}.000</h2>
-                    <div style='display: flex; flex-direction: row; gap: 0.8rem'>
-                        <button><i class="fa-solid fa-plus"></i></button>
-                        <p>1</p>
-                        <button><i class="fa-solid fa-minus"></i></button>
-                    </div>
-                    <i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i>
-                </div>`
-            );
-        }).join('');
-    }
-}
+// function displaycart() {
+//     let j = 0, total = 0;
+//     document.getElementById("cart-count").innerHTML = cart.length;
+//     if (cart.length == 0) {
+//         document.getElementById('cartItem').innerHTML = "The cart is empty";
+//         document.getElementById("total").innerHTML = "Rp " +0+ ".000";
+//         document.getElementById("cart-count").innerHTML = "";
+//     } else {
+//         document.getElementById('cartItem').innerHTML = cart.map((items) => 
+//         {
+//             var {image, title, price} = items;
+//             total=total+price;
+//             document.getElementById("total").innerHTML = "Rp " + total + ".000";
+//             return (
+//                 `<div class='cart-item'>
+//                     <div class='row-img'>
+//                         <img class='rowimg' src=${image}>
+//                     </div>
+//                     <p style='font-size: 15px;'>${title}</p>
+//                     <h2 style='font-size: 18px;'>Rp ${price}.000</h2>
+//                     <div style='display: flex; flex-direction: row; gap: 0.8rem'>
+//                         <button><i class="fa-solid fa-plus"></i></button>
+//                         <p>1</p>
+//                         <button><i class="fa-solid fa-minus"></i></button>
+//                     </div>
+//                     <i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i>
+//                 </div>`
+//             );
+//         }).join('');
+//     }
+// }
